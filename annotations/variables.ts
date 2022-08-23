fbf81f04b824b63eedbf83f8f3cb9c8e0f3d4fcc
Nic Vill <nicolas.villeneuve.01@gmail.com>
2022-08-23 12:08:17 -0400

    types.ts added

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,1 +1,25 @@
-const apples: number = 6;
+let apples: number = 6;
+let speed: string = 'fast';
+let hasName: boolean = false;
+let nothing: null = null;
+let undef: undefined = undefined;
+
+
+// arrays n stuff //
+let today: Date = new Date();
+let colors: string[] = ['red', 'blue', 'green'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true, true, false];
+
+// Classes //
+class Car {
+    make: 'Ford'
+}
+let car: Car = new Car();
+
+// Object literal //
+let point: { x: number; y: number } = {
+    x: 20,
+    y: 40
+};
+
